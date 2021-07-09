ec44ea6b9626ee35e42dcece0849e9cd01bef81a
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-07-09 11:50:18 +0530

    Excluded status bar

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, StatusBar } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 import BottomNavigation from "./src/navigation/BottomNavigation";
-import AwesomeIcon from "react-native-vector-icons/FontAwesome";
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
   return (
     <PaperProvider
       settings={{
-        icon: (props) => <AwesomeIcon {...props} />,
+        icon: (props) => <FontAwesomeIcon {...props} />,
       }}
     >
       <View style={styles.container}>
-        <StatusBar style="auto" />
         <BottomNavigation />
       </View>
     </PaperProvider>
@@ -23,6 +21,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: StatusBar.currentHeight,
   },
 });
